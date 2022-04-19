eb1173acf02d70f724d35f44c334ff654933af8d
Aditya Prakash <aditya.eth@gmail.com>
2022-04-20 01:15:20 +0530

    Testing variables

---
+++ Java/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name = "Aditya";
+        System.out.println(name);
+        int myNum = 69;
+        System.out.println(myNum);
+        String fName;
+        fName = "Aditya";
+        System.out.println(fName);
+        name = "bittu";
+        System.out.println(name);
+        final int cons = 6;
+        // cons = 8;
+        System.out.println(cons);
+        // More Variable types.
+        int myNumb = 65;
+        float myFloatNum = 5.99f;
+        String myString = "MyString";
+        char myChar = 'd';
+        boolean myBool = true;
+    }
+}
