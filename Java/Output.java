d990f148e75407206eec4bebd6e854732ef100ec
Aditya Prakash <aditya.eth@gmail.com>
2022-04-20 00:55:05 +0530

    First Output

---
+++ Java/Output.java
@@ -0,0 +1,11 @@
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I'm Learning java.");
+        System.out.println("It\'s Awesome");
+        System.out.println(3 + 3);
+        System.out.print("Hello World! ");
+        System.out.print("Let's print on same line.");
+
+    }
+}
