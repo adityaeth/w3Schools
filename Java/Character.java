fe562549b61f084a9dd6c45803a214bdea5a7356
Aditya Prakash <aditya.eth@gmail.com>
2022-04-20 01:40:51 +0530

    Tetsing char

---
+++ Java/Character.java
@@ -0,0 +1,8 @@
+public class Character {
+    public static void main(String[] args) {
+        char myGrade = 'A';
+        System.out.println(myGrade);
+        char myVar = 65;
+        System.out.println(myVar);
+    }
+}
