2e0927bbfb787203f1063f1f2590da0921733fb7
Aditya Prakash <aditya.eth@gmail.com>
2022-04-20 01:21:54 +0530

    Testing both text and variable combinations

---
+++ Java/PrintVariable.java
@@ -0,0 +1,12 @@
+public class PrintVariable {
+    public static void main(String[] args) {
+        String fName = "Aditya ";
+        String lName = "Prakash";
+        String fullName = fName + lName;
+        System.out.println(fName + lName);
+        System.out.println("Full Name: " + fullName);
+
+        int x = 6, y = 9;
+        System.out.println(x + y);
+    }
+}
