42ef2fcaec987ae061fadd446a3c6b316d4fecd9
Aditya Prakash <aditya.eth@gmail.com>
2022-04-20 01:12:15 +0530

    testing comments

---
+++ Java/Comment.java
@@ -0,0 +1,11 @@
+public class Comment {
+    public static void main(String[] args) {
+        // This is a comment.
+        System.out.println("Testing Comments."); // This is also a comment.
+        /*
+         * This is a multi line comment.
+         * This is the second line.
+         */
+        System.out.println("Testing multi line comments.");
+    }
+}
