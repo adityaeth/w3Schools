f95bd2752aeb3d50a547ca5af42d695029081af0
Aditya Prakash <aditya.eth@gmail.com>
2022-04-20 18:41:45 +0530

    Testing Math

---
+++ Java/MathClass.java
@@ -0,0 +1,12 @@
+public class MathClass {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(5, 10));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-64.985));
+        System.out.println(Math.random());
+        System.out.println(Math.random() * 101);
+        int randNo = (int) (Math.random() * 101);
+        System.out.println(randNo);
+    }
+}
